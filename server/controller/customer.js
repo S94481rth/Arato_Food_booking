98782fc92b2ecba3aa5ea0da00c04a2cc3a9aa05
Aravind <aravind.cs20@bmsce.ac.in>
2023-11-09 19:40:36 +0530

    controller and model to support basic app

--- server/controller/customer.js
+++ server/controller/customer.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const userModel = require('../models/customer');
+const Customer = userModel.Customer;
+
 
 exports.welcome = async (req,res) => {
     res.send("welcome to the great kirikaalan magic show");
@@ -9,4 +10,21 @@ exports.getUsers = async (req,res) => {
     const result = await userModel.getUsers();
     console.log(result);
     res.json(result);
+}
+
+exports.getUser = async (req,res) => {
+    const id = req.params.id;
+    console.log(id);
+    const result = await userModel.getUser(id);
+    res.json(result);
+}
+
+exports.insertUser = async (req, res) => {
+    const name = req.body.name;
+    const city = req.body.city;
+    const phno = req.body.phno;
+    //const boss = req.body.boss;
+    console.log(req.body);
+    const result = await userModel.insertUser(new Customer(name,city, phno));
+    res.status(200).send(req.body);
 }
--- server/models/customer.js
+++ server/models/customer.js
@@ -1,10 +1,64 @@
 const db = require('../middleware/db');
 
-exports.getUsers = () => {
-    query = "SELECT * FROM customer";
-    db.query(query, (err, result) => {
-        if (err) throw err;
-        //console.log(result);
-        return result;
+const getUsers = () => {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM customer";
+        db.query(query, (err, result) => {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        });
     });
+}
+
+const getUser = (id) => {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM customer WHERE id = ?";
+        db.query(query, [id], (err, result) => {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        });
+    });
+}
+
+const insertUser = (customer) => {
+    const name = customer.name;
+    const city = customer.city;
+    const phno = customer.phno;
+//    const boss = customer.boss;
+
+    return new Promise((resolve, reject) => {
+        const query = "insert into customer( name, city, phno) values (?, ?, ?);"
+        db.query(query, [name,city, phno], (err, result) => {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(result);
+            }
+        });
+    });
+}
+
+class Customer{
+    constructor(name, city, phno){
+        this.name = name;
+        this.city =  city;
+        this.phno = phno;
+        //this.boss = boss;
+    }
+}
+
+module.exports = {
+    Customer,
+    getUser,
+    getUsers,
+    insertUser
 }
