4a8e1ff3c87d588ba06461eea320feb0dfd267e9
Aravind <aravind.cs20@bmsce.ac.in>
2023-11-09 19:39:58 +0530

    basic working server.js

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
 const PORT = process.env.PORT_NUMBER || 3000;
 
 // console.log(process.env.DB_USER);
 
-
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const customerRoutes = require('./server/routes/customer');
+app.use("/customer", customerRoutes);
 
 app.listen(PORT, () => {
     console.log(`running on port number ${PORT}`);
